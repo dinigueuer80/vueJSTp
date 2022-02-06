32bff7ee1462452d3274aea599fc275c8c52bd5f
Constant YATTA <yattadeogracias@gmailcom>
2022-02-06 20:14:57 +0100

    06_jan_22

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  //import SignUp from './components/SignUp.vue'
+  export default {
+    name: 'App',
   }
-}
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Dashboard</h1>
+    <p>Liste des Utilisateurs</p>
+    <p>
+        <router-link to="/sign-up">Nouveau Utilisateur</router-link>
+    </p>
+</template>
+
+<script>
+    //import axios from 'axios'
+    export default {
+        name : 'Home'
+    }
+</script>
+
